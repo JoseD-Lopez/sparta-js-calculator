3351192322a1496186de4ea2ea72dd2225cdf6fd
Tech-A11 <tech-a11@Tech-A11s-MacBook-Air.local>
2018-02-19 22:33:34 +0000

    created the BIM Calculators

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 
-var calcChoice = prompt('Would you like to use the basic or the advance calculator? for Advance press 1 for Basic press 2');
+var calcChoice = prompt('Would you like to use the basic or the advance calculator? for Advance press 1 for Basic press 2 and press 3 to calculate your BMI');
 
 if(calcChoice == 1){
 
@@ -30,28 +30,68 @@ alert('You have chosen the basic level calculator');
 var number1 = prompt('Please enter the first number you want to perform calculation on');
 var number2 = prompt('Please enter the second number you want to perfom a calculation on');
 	
+	var x = parseInt(number1);
+	var y = parseInt(number2);
 var typeofCalculation = prompt('For addition press 1, for multiplication press 2, for division press 3, for subtraction press 4');
  
  switch(typeofCalculation){
  	case '1':
- 	var answer = parseInt(number1) + parseInt(number2);
+ 	var answer = x + y;
  	alert(answer);
  	break;
  	case '2':
- 	var answer = parseInt(number1) * parseInt(number2);
+ 	var answer = x * y;
  	alert(answer);
  	break;
  	case '3':
- 	var answer = parseInt(number1) / parseInt(number2);
+ 	var answer = x / y;
  	alert(answer);
  	break;
  	case '4':
- 	var answer = parseInt(number1) - parseInt(number2);
+ 	var answer = x - y
  	alert(answer);
  	break;
  }
+
 }
-else if(calcChoice === 3) {
+else if(calcChoice == 3) {
+	alert('You have chosen the BMI calculator');
+
+var imperialOrMetric = prompt('Please press 1 for imperial or 2 for metric system');
+
+if(imperialOrMetric == 1){
+
+var heightfeet = prompt('Please Enter your height in feet');
+var additionalInches = prompt('Please enter any additional inches');
+var weightpounds = prompt('Please Enter your weight pounds');
+
+var calcInches = parseInt(additionalInches) + parseInt(heightfeet) * 12;
+
+var answer =  (parseInt(weightpounds) / (calcInches * calcInches)) * 703;
+
+alert('Your BMI is : '+ answer.toFixed(2));
+
+} 
+
+else if(imperialOrMetric == 2){
+var weightkg = prompt('Please Enter your weight KG');
+var heightmtrs = prompt('Please Enter your height in meters');
+
+var weight = parseFloat(weightkg);
+var height = parseFloat(heightmtrs);
+
+var heightPow2 = Math.pow(height, 2);
+
+var answer = weight / heightPow2;
+
+alert('Your BMI is: ' + answer.toFixed(2));
+}
+}
+else{
 	alert('exit');
-	
 }
+
+
+
+
+
